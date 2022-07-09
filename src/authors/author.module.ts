8bc7915b14cee35aa0382c34810d0fbf59192026
Daniel <daniel@exnaton.com>
2022-07-09 17:31:45 +0200

    author model added

--- src/authors/author.module.ts
+++ src/authors/author.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './services/author.service';
 import { AuthorController } from './controllers/author.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [AuthorController],
-  providers: [AuthorService],
+  providers: [AuthorService, PrismaService],
 })
 export class AuthorModule {}
--- src/authors/services/author.service.ts
+++ src/authors/services/author.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthorDto } from '../dto/create-author.dto';
 import { UpdateAuthorDto } from '../dto/update-author.dto';
+import { PrismaService } from '../../prisma.service';
 
 @Injectable()
 export class AuthorService {
+  constructor(private prismaService: PrismaService) {}
   create(createAuthorDto: CreateAuthorDto) {
     return 'This action adds a new author';
   }
 
   findAll() {
-    return `This action returns all author`;
+    return this.prismaService.author.findMany();
   }
 
   findOne(id: number) {
